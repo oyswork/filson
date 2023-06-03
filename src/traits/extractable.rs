076517042ff3d235974a160ec07e3c2c365b8cb5
A.S <oyswork@gmail.com>
2023-06-03 20:34:15 +0200

    code examples formatting

--- src/traits/extractable.rs
+++ src/traits/extractable.rs
@@ -6,35 +6,72 @@ pub trait Extractable {
     /// In order to do that you have to instruct Filson how to retrieve the required fields and convert them into [DataNode]s, which is the data format Filson understands.
     ///
     /// ```rust
-    ///     use filson::{Appliable, Extractable, DataNode, get_filter, FilsonError};
+    /// use filson::{get_filter, Appliable, DataNode, Extractable, FilsonError};
     ///
-    ///     #[derive(Debug, PartialEq)]
-    ///     struct Car {
-    ///         make: &'static str,
-    ///         age: u8,
-    ///         milage: u64,
-    ///     }
+    /// #[derive(Debug, PartialEq)]
+    /// struct Car {
+    ///     make: &'static str,
+    ///     age: u8,
+    ///     milage: u64,
+    /// }
     ///
-    ///     let cars = vec![Car{ make: "Ford", age: 7, milage: 100000 }, Car{ make: "Volvo", age: 3, milage: 50000 }, Car{make: "Volvo", age: 4, milage: 47000}];
+    /// let cars = vec![
+    ///     Car {
+    ///         make: "Ford",
+    ///         age: 7,
+    ///         milage: 100000,
+    ///     },
+    ///     Car {
+    ///         make: "Volvo",
+    ///         age: 3,
+    ///         milage: 50000,
+    ///     },
+    ///     Car {
+    ///         make: "Volvo",
+    ///         age: 4,
+    ///         milage: 47000,
+    ///     },
+    /// ];
     ///
-    ///     // let's implement the Extractable so, that Filson knows how to extract age and make data from Car structs
-    ///     impl Extractable for Car {
-    ///         fn extract(&self, path: &str) -> Result<DataNode, FilsonError> {
-    ///             match path {
-    ///                 "make" => Ok(self.make.into()),
-    ///                 // DataNode only accepts i64 integers so the explicit conversion is required
-    ///                 "age" => Ok((self.age as i64).into()),
-    ///                 // We don't need to be able to extract milage field
-    ///                 _ => Err(FilsonError::ExtractionError),
-    ///             }
+    /// // let's implement the Extractable so,
+    /// // that Filson knows how to extract age and make data from Car structs
+    /// impl Extractable for Car {
+    ///     fn extract(&self, path: &str) -> Result<DataNode, FilsonError> {
+    ///         match path {
+    ///             "make" => Ok(self.make.into()),
+    ///             // DataNode only accepts i64 integers so the explicit conversion is required
+    ///             "age" => Ok((self.age as i64).into()),
+    ///             // We don't need to be able to extract milage field
+    ///             _ => Err(FilsonError::ExtractionError),
     ///         }
     ///     }
+    /// }
     ///
-    ///     // Now we ready to build the Filson query and run it over our data
-    ///     let query = r#"and(compare("make" == "Volvo"), compare("age" < 5))"#;
-    ///     let flt = get_filter(query).unwrap();
-    ///     let filtered_cars = cars.into_iter().filter(|car| flt.apply(car).unwrap_or(false)).collect::<Vec<_>>();
-    ///     assert_eq!(filtered_cars, vec![ Car{ make: "Volvo", age: 3, milage: 50000 }, Car{make: "Volvo", age: 4, milage: 47000}]);
+    /// // Now we ready to build the Filson query and run it over our data
+    /// // Remember, that query may be known only at runtime
+    /// let query = r#"and(compare("make" == "Volvo"), compare("age" < 5))"#;
+    /// let flt = get_filter(query).unwrap();
+    ///
+    /// let filtered_cars = cars
+    ///     .into_iter()
+    ///     .filter(|car| flt.apply(car).unwrap_or(false))
+    ///     .collect::<Vec<_>>();
+    ///
+    /// assert_eq!(
+    ///     filtered_cars,
+    ///     vec![
+    ///         Car {
+    ///             make: "Volvo",
+    ///             age: 3,
+    ///             milage: 50000
+    ///         },
+    ///         Car {
+    ///             make: "Volvo",
+    ///             age: 4,
+    ///             milage: 47000
+    ///         }
+    ///     ]
+    /// );
     /// ```
     fn extract(&self, path: &str) -> Result<DataNode, FilsonError>;
 }
